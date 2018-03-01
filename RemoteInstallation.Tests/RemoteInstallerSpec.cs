f5c39f2cc234908359ed8c68bb94a702b33ca70e
Radek Falhar <Radek.Falhar@hotmail.com>
2018-03-01 19:09:45 +0100

    Vytvoření solution a základních projektů.

---
+++ RemoteInstallation.Tests/RemoteInstallerSpec.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace RemoteInstallation.Tests
+{
+    public class RemoteInstallerSpec
+    {
+        [Test]
+        public void Test()
+        {
+            RemoteInstaller ri = new RemoteInstaller();
+            Assert.IsNotNull(ri);
+        }
+    }
+}
---
+++ RemoteInstallation/RemoteInstaller.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace RemoteInstallation
+{
+    public class RemoteInstaller
+    {
+
+    }
+}
